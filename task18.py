547419f8d4c2ab73f4d1a441e598c284b7d05e33
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-09-29 10:30:46 +0500

    ok

--- task18.py
+++ task18.py
@@ -1,2 +1,35 @@
-def group_products_by_category(products: list[dict]) -> dict[str, list[dict]]:
-    pass
+def group_products_by_category(products: list[dict]) -> dict[str, list[str]]:
+    grouped = {}
+    for p in products:
+        cat = p["category"]
+        if cat not in grouped:
+            grouped[cat] = []
+        grouped[cat].append(p["name"])
+    return grouped
+products = [
+    {"name": "iPhone 15", "price": 1200, "category": "Electronics"},
+    {"name": "Samsung TV", "price": 800, "category": "Electronics"},
+    {"name": "Nike Shoes", "price": 150, "category": "Fashion"},
+    {"name": "Adidas Shoes", "price": 120, "category": "Fashion"},
+    {"name": "Couch", "price": 500, "category": "Furniture"},
+    {"name": "Chair", "price": 100, "category": "Furniture"},
+    {"name": "MacBook Pro", "price": 2000, "category": "Electronics"},
+    {"name": "T-shirt", "price": 25, "category": "Fashion"},
+    {"name": "Desk", "price": 220, "category": "Furniture"},
+    {"name": "Monitor", "price": 300, "category": "Electronics"},
+    {"name": "Keyboard", "price": 50, "category": "Electronics"},
+    {"name": "Mouse", "price": 30, "category": "Electronics"},
+    {"name": "Jeans", "price": 80, "category": "Fashion"},
+    {"name": "Jacket", "price": 200, "category": "Fashion"},
+    {"name": "Bed", "price": 700, "category": "Furniture"},
+    {"name": "Lamp", "price": 60, "category": "Furniture"},
+    {"name": "iPad", "price": 900, "category": "Electronics"},
+    {"name": "Watch", "price": 250, "category": "Fashion"},
+    {"name": "Bookshelf", "price": 400, "category": "Furniture"},
+    {"name": "Headphones", "price": 180, "category": "Electronics"},
+]
+
+grouped = group_products_by_category(products)
+
+for cat, names in grouped.items():
+    print(cat, ":", names)
--- task19.py
+++ task19.py
@@ -1,2 +1,36 @@
-def get_products_in_price_range(products: list[dict], min_price: float, max_price: float) -> list[dict]:
-    pass
+def get_products_in_price_range(products):
+    min_price=products[0]
+    max_price=products[0]
+    for product in products:
+        if product["price"] < min_price["price"]:
+            min_price = product
+        if product["price"] > max_price["price"]:
+            max_price = product
+
+    return min_price, max_price        
+
+products = [
+    {"name": "iPhone 15", "price": 1200, "category": "Electronics"},
+    {"name": "Samsung TV", "price": 800, "category": "Electronics"},
+    {"name": "Nike Shoes", "price": 150, "category": "Fashion"},
+    {"name": "Adidas Shoes", "price": 120, "category": "Fashion"},
+    {"name": "Couch", "price": 500, "category": "Furniture"},
+    {"name": "Chair", "price": 100, "category": "Furniture"},
+    {"name": "MacBook Pro", "price": 2000, "category": "Electronics"},
+    {"name": "T-shirt", "price": 25, "category": "Fashion"},
+    {"name": "Desk", "price": 220, "category": "Furniture"},
+    {"name": "Monitor", "price": 300, "category": "Electronics"},
+    {"name": "Keyboard", "price": 50, "category": "Electronics"},
+    {"name": "Mouse", "price": 30, "category": "Electronics"},
+    {"name": "Jeans", "price": 80, "category": "Fashion"},
+    {"name": "Jacket", "price": 200, "category": "Fashion"},
+    {"name": "Bed", "price": 700, "category": "Furniture"},
+    {"name": "Lamp", "price": 60, "category": "Furniture"},
+    {"name": "iPad", "price": 900, "category": "Electronics"},
+    {"name": "Watch", "price": 250, "category": "Fashion"},
+    {"name": "Bookshelf", "price": 400, "category": "Furniture"},
+    {"name": "Headphones", "price": 180, "category": "Electronics"},
+]
+result=get_products_in_price_range(products)
+print("Eng kichik narx  :", result[0])
+print("Eng katta narx:", result[1])
